0b00e026706f3d03057908a21cb41e84db2fd221
Levi4728 <llb.broughton@gmail.com>
2025-05-05 18:04:07 +0000

    w5 Bluey and Bingo

--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -10,6 +10,29 @@ directory ) and moves to the corners of the screen in a square pattern.
 
 
 import turtle
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=600, height=600)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "emoji.png") # Set the background image of the screen
 
 
 def set_turtle_image(turtle, image_name):
@@ -28,16 +51,19 @@ screen = turtle.Screen()
 screen.setup(width=600, height=600)
 
 # Create a turtle and set its shape to the custom GIF
-t = turtle.Turtle()
-
-set_turtle_image(t, "Bluey image GIF.gif")
-
-t.penup()
-t.speed(3)
-
+b = turtle.Turtle()
+bi = turtle.Turtle()
+set_turtle_image(b, "Bluey image GIF.gif")
+set_turtle_image(bi, "Bingo image GIF.gif")
+b.penup()
+b.speed(1)
+bi.penup()
+bi.speed(1)
 for i in range(4):
-    t.goto(200, 200)
-    t.goto(-200, -100)
+    b.goto(50, 50)
+    b.goto(-50, -50)
+    bi.goto(100, 100)
+    bi.goto(-100,100)
 
 
 turtle.exitonclick()
